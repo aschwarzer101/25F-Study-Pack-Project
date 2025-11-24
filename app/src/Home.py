8f1c82d9d6e41a0bfc63d02a81da19900ddc95db
aschwarzer101 <124056235+aschwarzer101@users.noreply.github.com>
2025-11-24 08:59:31 -0500

    changinge buttons names test part 2.

--- app/src/Home.py
+++ app/src/Home.py
@@ -56,7 +56,7 @@ if st.button("Act as Anna Smith, a Northeastern professor",
     st.session_state['first_name'] = 'John'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
+    logger.info("Logging in as Anna Smith, Professor Data Administrator Persona")
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
 if st.button('Act as Mohammad, an USAID worker', 
